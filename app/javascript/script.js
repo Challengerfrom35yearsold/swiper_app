c7758e5fc1480f3301f77309bf8d08878c1317a8
Challengerfrom35yearsold <124878663+Challengerfrom35yearsold@users.noreply.github.com>
2023-11-22 03:00:23 +0000

    [Finish]JavaScriptを学ぼう,13章【RailsにJS,jQueryを実装しよう】

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -7,3 +7,21 @@ $(function () {
     $(this).css('color','red');
   });
 });
+
+// Swiperのオプションを定数化
+const opt = {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+};
+
+// Swiperを実行(初期化)
+/*global Swiper*/
+$(document).on('turbolinks:load', function() {
+    let swiper = new Swiper('.swiper',opt);
+});
